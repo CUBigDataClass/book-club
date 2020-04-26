f209160c88e16a8c0e47e5a3827c5d78aa19a9ae
Alex Book <alexbook00@gmail.com>
2020-04-26 02:11:00 -0500

    highlighting and county name popups

--- map.py
+++ map.py
@@ -4,6 +4,9 @@ import folium
 import json
 import webbrowser
 import branca
+from datetime import datetime, timedelta
+from pandas.io.json import json_normalize
+import branca.colormap as cm
 
 # create pandas dataframe with covid case/death data and FIPS info
 county_data = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
@@ -30,9 +33,11 @@ df['FIPS_Code'] = df['FIPS_Code'].astype(float).astype(int).astype(str)
 # add this part with front end linkage
 # set "today" as chosen date
 
-today = '2020-04-20'
+df['date'] = pd.to_datetime(df['date'])
+recent_date = df['date'].max()
+chosen_date = str(recent_date)
 
-df = df[df.date == today]
+df = df[df.date == chosen_date]
 df['date'] = df['date'].astype(str)
 
 #########################
@@ -40,14 +45,27 @@ df['date'] = df['date'].astype(str)
 counties = 'us-counties.json'
 
 # bins to control the color-coding
-# bins = list(df['cases'].quantile([0, .5, .75, .8, .85, .997, .998, .999, 1]))
-bins = list([0, 10, 100, 1000, 10000, 100000, 200000])
+max_cases = df['cases'].max()
+bins = list([0, max_cases*.0001, max_cases*.001, max_cases*.01, max_cases*.1, max_cases])
 
 m = folium.Map(location = [39.8282, -98.5795], zoom_start = 4)
 
-folium.Choropleth(counties, data = df, columns = ['FIPS_Code', 'cases'],
-                  key_on = 'feature.id', fill_color = 'YlOrRd', fill_opacity = 0.7, line_opacity = 0.5,
-                  legend_name = 'COVID-19 cases', bins = bins, reset = True).add_to(m)
+choropleth = folium.Choropleth(counties, data = df, columns = ['FIPS_Code', 'cases'],
+                  key_on = 'feature.id', fill_color = "YlOrRd", fill_opacity = 0.7, line_opacity = 0.5,
+                  legend_name = 'COVID-19 cases', highlight = True, bins = bins, reset = True)
+
+choropleth.geojson.add_child(folium.features.GeoJsonTooltip(['name'], aliases = ['County:']))
+
+choropleth.add_to(m)
+
+#########################
+# create markers for each county
+# json_df = pd.read_json(counties)
+# json_df['features'] = json_df['features'].astype(str)
+# json_df.features.str.split(expand=True)
+# print(json_df)
+
+#########################
 
 m.save('new.html')
 webbrowser.open('new.html')
